2a2449762b1ad35e94f98f2db320502da268296a
Wendy Palomino <wvanessa.palomino.rivera@gmail.com>
2020-09-18 19:07:53 -0400

    added about category

---
+++ lib/pages/screen_about/widgets/about_category.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:pokemon_app/models/pokemon_class.dart';
+
+
+class AboutCategory extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const AboutCategory({Key key, this.pokemon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Container(
+            child: Text( "Bulbasaur is a small, quadruped Pokémon that has blue-green skin with darker patches. It has red eyes with white pupils, pointed, ear-like structures on top of ile l",),
+          ),
+          SizedBox(height: 30),
+          Container(
+            height: 70,
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(
+                color: Colors.grey[200],
+                blurRadius: 10,
+                offset: Offset(0.0, 0.75)
+              )],
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Height', style: TextStyle( color: Colors.grey),),
+                  Text('743290749'),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Weight',style: TextStyle( color: Colors.grey),),
+                  Text('743290749'),
+                ],
+              )
+            ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/screen_about/widgets/categories.dart
+++ lib/pages/screen_about/widgets/categories.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_app/pages/screen_about/widgets/about_category.dart';
 
 class Categories extends StatefulWidget {
+  
   @override
   _CategoriesState createState() => _CategoriesState();
 }
@@ -10,16 +12,21 @@ class _CategoriesState extends State<Categories> {
   int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      child: SizedBox(
-        height: 25,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (context, index) => buildCategory(index),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: SizedBox(
+            height: 25,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) => buildCategory(index),
+            ),
+          ),
         ),
-      ),
+        AboutCategory(),
+      ],
     );
   }
 
@@ -46,7 +53,7 @@ class _CategoriesState extends State<Categories> {
               margin: EdgeInsets.only(top: 20 / 4), //top padding 5
               height: 2,
               width: 30,
-              color: selectedIndex == index ? Colors.black : Colors.transparent,
+              color: selectedIndex == index ? Color(0xff567DF4) : Colors.transparent,
             )
           ],
         ),
